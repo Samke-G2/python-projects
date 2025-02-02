5ca1403f481876998b47c9361782ff3db726657e
Samkelwa <samkelwa101@gmail.com>
2025-02-02 21:54:24 +0200

    initial commit into projects repo, the simple bot with 3 inputs and 4 responses.

---
+++ simple_bot.py
@@ -0,0 +1,16 @@
+# Bot project               30/01/2025                  17:44
+
+name = input("Hello! What is your name? ")
+print(f"Nice to meet you, {name}!")
+
+age_input = input("How old are you? ")
+age = int(age_input)
+
+bot_age = 3
+age_difference = age - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+
+colour = input("What's your favourite colour? ")
+print(f"Oh, {colour} is a beautiful colour! ")
+
+print(f"Thanks for the conversation, {name}")
