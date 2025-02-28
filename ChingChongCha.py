40292179433b390144fe8b81226498615155e807
Samkelwa <samkelwa101@gmail.com>
2025-02-28 20:30:08 +0200

    Added starting notes to ChingChongCha

--- ChingChongCha.py
+++ ChingChongCha.py
@@ -1,2 +1,21 @@
 # Rock, Paper, Scissors (AKA "Ching Chong Cha", from obscure childhood memories ig)                     28/02/2025                      17:26
 
+# User input from teh first user ( the computer, ideally)
+
+
+# User input from teh second user (person playing the game vs computer)
+
+
+# if statements that compare the choices (in a function, we know from 'calculator' that free-standing if satements don't really work well)
+# OR I could assign the possible numbers some number values and compare those (dunno how, just seems like it would be interesting to do)
+
+# Nine possible outcomes, i think
+
+
+
+
+# After i've created this one, maybe create a best-out-of-three version??
+
+
+
+
