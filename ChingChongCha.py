5a469a4d8f076b136c84428bee9ec6f6c205eda6
Samkelwa <samkelwa101@gmail.com>
2025-03-05 16:38:07 +0200

    Added 2 functions to ChingChongCha, now it works, and it repeats if there is a tie.

--- ChingChongCha.py
+++ ChingChongCha.py
@@ -1,6 +1,6 @@
 # Rock, Paper, Scissors (AKA "Ching Chong Cha", from obscure childhood memories ig)                     28/02/2025                      17:26
 
-# User input from teh first user ( the computer, ideally)
+# User input from the first user ( the computer, ideally)
 computer = "Rock"
 
 # User input from teh second user (person playing the game vs computer)
@@ -10,15 +10,45 @@ player_1 = input("What's your choice?: ")
 # OR I could assign the possible numbers some number values and compare those (dunno how, just seems like it would be interesting to do)
 
 # Nine possible outcomes, i think
-def game(xer, yer):
+def game1(xer, yer):
     if xer == yer:
         print("It's a tie.")
-        # Then another function for repeating, maybe
+        player_1 = input("What's your choice?: ")
+        game2(computer, player_1)
     elif  xer == "Paper" and yer == "Rock":
         print(f"{xer} wins! ")
-    elif xer == "Paper" and yer == "Scissors":a
+    elif xer == "Paper" and yer == "Scissors":
+        print(f"{yer} wins! ")
+    elif xer == "Rock" and yer == "Paper":
+        print(f"{yer} wins! ")
+    elif xer == "Rock" and yer == "Scissors":
+        print(f"{xer} wins! ")
+    elif xer == "Scissors" and yer == "Paper":
+        print(f"{xer} wins! ")
+    elif xer == "Scissors" and yer == "Rock":
+        print(f"{yer} wins! ")
+        
+def game2(xer, yer):
+    if xer == yer:
+        print("It's a tie.")
+        player_1 = input("What's your choice?: ")
+        game1(computer, player_1)
+    elif  xer == "Paper" and yer == "Rock":
+        print(f"{xer} wins! ")
+    elif xer == "Paper" and yer == "Scissors":
+        print(f"{yer} wins! ")
+    elif xer == "Rock" and yer == "Paper":
+        print(f"{yer} wins! ")
+    elif xer == "Rock" and yer == "Scissors":
+        print(f"{xer} wins! ")
+    elif xer == "Scissors" and yer == "Paper":
+        print(f"{xer} wins! ")
+    elif xer == "Scissors" and yer == "Rock":
+        print(f"{yer} wins! ")
 
 
+# run the if-statement function with the input value(s)
+game2(computer, player_1)
 
 # After i've created this one, maybe create a best-out-of-three version??
 
