daa716871b071807db02adc2f5789f743d367dc5
Samkelwa <samkelwa101@gmail.com>
2025-03-05 17:48:21 +0200

    Added a new file, a hangman project. Only the instructional comments tho

---
+++ hangman.py
@@ -0,0 +1,13 @@
+# Hangman Project                               # 03/05/2025                                    17:21
+
+# First, I'll have a word
+
+
+# Next, I'll have user input guessing that word
+
+
+# Then a function to check if the user's word is my word
+# If not, the function asks the user to guess a letter in my word, then checks if the letter is present.
+# If the letter is present, the program will say so and show the position of the letter
+# If not it will increment a count,
+# when the count reaches 6 the user will lose the game and the word will be displayed
