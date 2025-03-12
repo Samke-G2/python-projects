6b1035adeb52926b9a86b0715de660b45c8154f9
Samkelwa <samkelwa101@gmail.com>
2025-03-12 18:12:04 +0200

    Refined the number guesser to its final form, it now handles errors and counts tries

--- guess_number.py
+++ guess_number.py
@@ -2,24 +2,34 @@
 import random
 
 # intro the concept of the number guesser
+print(" ")
 print("Can you guess the whole number, between 1 and 100, that I'm thinking about?")
 number = random.randint(1, 100)
 
-# Start with user input
-guess = int(input("What's your guess? "))
-print("  ")
+tries = 0
 
-# While loop displays as long as the number is incorrect
-while guess != number:
-    
-    if guess > number:
-        print("That's not it. Guess lower.")
-        guess = int(input("Try again: "))
-        print("  ")
-    elif guess < number:
-        print("That's not it. Guess higher.")
-        guess = int(input("Try again: "))
-        print("  ")
+while True:
+    try:
+        # Start with user input
+        guess = int(input("What's your guess? "))
+        print(" ")
         
-# print statement for when number is correct        
-print("WELL DONE! YOU GUESSED MY NUMBER :D ")
+        # While loop displays as long as the number is incorrect
+            
+        if guess > number:
+            print("That's not it. Guess lower.")
+            print("  ")
+            tries += 1
+        elif guess < number:
+            print("That's not it. Guess higher.")
+            print("  ")
+            tries += 1
+        else:
+            # print statement for when number is correct        
+            print("WELL DONE! YOU GUESSED MY NUMBER :D ")
+            tries += 1
+            break   
+    except ValueError:
+        print("Please enter a valid number")
+        
+print(f"It only took you {tries} tries.")
