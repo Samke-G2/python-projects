6d5914c6aa22755a3c9b92635c0e1eb8ce229fa6
Samkelwa <samkelwa101@gmail.com>
2025-09-11 16:29:32 +0200

    created a file for the nine lives project and added a lives variable to the function and a words list.

---
+++ nine_lives.py
@@ -0,0 +1,16 @@
+"""
+nine_lives.py
+author: samke_g2
+
+similar to hangman, but a different implementation
+"""
+def nineLives():
+    lives = 9
+
+    words = (
+        "Aardvark", "alligator", "ant", "Apple", "Orange", "Banana", "Coconut", "Pineapple",
+        "ape", "Life", "loser", "game", "play", "television", "work", "python",
+        "hippopotamus", "parrot", "jaguar", "monkey", "awe", "bread", "Winner", "break"
+    )
+
+    
